f4b0448b910a391737e165587daec9d6209743a2
oles <olchyk89@gmail.com>
2018-11-18 11:02:29 +0100

    INITIAL COMMIT

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+
+class App extends React.Component {
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default App;
